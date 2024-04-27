ec3b4e5875e3000d3730213cebbf7a20c002fd95
Lekan <jimohridwanlekan@gmail.com>
2024-04-27 18:52:40 +0100

    Add marketing page

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate200 px-4">
-      Header
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src="https://assets.website-files.com/5f6ab89b45559536fe7e0394/5f6bcbf217bcd418d7310a6b_home%20duo%20jumping.svg"
+            alt="logo"
+            height={40}
+            width={40}
+          />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="w-5 h-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              afterSignInUrl="/learn"
+              afterSignUpUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   );
 };
